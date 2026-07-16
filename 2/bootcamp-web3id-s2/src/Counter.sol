50504303682cfde8de68ce4c21fb32600472f841
peenjeee <gastwocls02@gmail.com>
2026-07-16 23:07:08 +0700

    feat: initialize Foundry project with RewardToken contract and suite of tests

---
+++ 2/bootcamp-web3id-s2/src/Counter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Counter {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
